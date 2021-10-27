c051c17238cad4cea48e07a7312ce698cd3bbb13
Just15 <Justintwood15@outlook.com>
2021-10-26 18:53:48 -0600

    Add console, basic structure

---
+++ src/GitHubReleaseNotesGenerator/GitHubReleaseNotesGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Octokit;
+
+namespace GitHubReleaseNotesGenerator
+{
+    // https://octokitnet.readthedocs.io/en/latest/getting-started/
+
+    public class GitHubReleaseNotesGenerator
+    {
+        public string RepositoryOwner { get; private set; }
+        public string RepositoryName { get; private set; }
+
+        private GitHubClient gitHubClient;
+
+        public GitHubReleaseNotesGenerator(string repositoryOwner, string repositoryName, Credentials credentials)
+        {
+            RepositoryOwner = repositoryOwner;
+            RepositoryName = repositoryName;
+
+            gitHubClient = new GitHubClient(new ProductHeaderValue(repositoryName))
+            {
+                Credentials = credentials
+            };
+        }
+    }
+}
