b8ac4cc640126291dd3a88f5a6018e50a8e371e1
Just15 <Just15@users.noreply.github.com>
2021-10-27 22:11:09 -0600

    Remove trying to use generate-notes

--- src/GitHubReleaseNotesGenerator.ConsoleApp/Program.cs
+++ src/GitHubReleaseNotesGenerator.ConsoleApp/Program.cs
@@ -1,32 +1,15 @@
 ﻿using Octokit;
-using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using System.Threading.Tasks;
 
 namespace GitHubReleaseNotesGenerator.ConsoleApp
 {
     internal class Program
     {
-        private static async Task Main(string[] args)
+        private static void Main(string[] args)
         {
             var gitHubReleaseNotesGenerator = new GitHubReleaseNotesGenerator(
                 "Just15",
                 "PdfSharpWrapper",
                 new Credentials(""));
-
-            HttpClient gitHubClient = new HttpClient
-            {
-                //BaseAddress = new Uri("https://api.github.com/repos/octocat/")
-                BaseAddress = new Uri("https://api.github.com/repos/"),
-                DefaultRequestHeaders =
-                {
-                    Authorization = new AuthenticationHeaderValue("Bearer", "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")
-                }
-            };
-
-            await gitHubReleaseNotesGenerator.DoStuff();
         }
     }
 }
--- src/GitHubReleaseNotesGenerator/GitHubReleaseNotesGenerator.cs
+++ src/GitHubReleaseNotesGenerator/GitHubReleaseNotesGenerator.cs
@@ -1,25 +1,13 @@
 ﻿using Octokit;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace GitHubReleaseNotesGenerator
 {
     // https://octokitnet.readthedocs.io/en/latest/getting-started/
 
-    // https://docs.github.com/en/rest/reference/repos#generate-release-notes-content-for-a-release
-
-    // Get repository ID, maybe
-    // https://codesnippet.io/github-api-tutorial/
-
     public class GitHubReleaseNotesGenerator
     {
         public string RepositoryOwner { get; private set; }
         public string RepositoryName { get; private set; }
-        public string PostGenerateNotesUri
-        {
-            get { return $"{RepositoryOwner}/{RepositoryName}/releases/generate-notes"; }
-        }
 
         private GitHubClient gitHubClient;
 
@@ -32,16 +20,6 @@ namespace GitHubReleaseNotesGenerator
             {
                 Credentials = credentials
             };
-        }
-
-        public async Task DoStuff()
-        {
-            var repository = await gitHubClient.Repository.Get("Just15", "GitVersion-PdfSharpWrapper");
-
-            // Post - /repos/{owner}/{repo}/releases/generate-notes
-            var uriString = $"https://api.github.com/repos/{PostGenerateNotesUri}";
-            IApiResponse<HttpResponseMessage> apiResponse = await gitHubClient.Connection.Post<HttpResponseMessage>(new Uri(uriString),
-                new GenerateNotes("1.0.5") , "application/json", "application/json");
         }
     }
 }
